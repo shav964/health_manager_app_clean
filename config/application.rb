096aa7da8399254f03ebd2bfad153f7880dc0a50
aria8686 <ariados0618@gmail.com>
2024-12-29 11:57:52 +0900

    日本時間に設定

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module HealthManagerApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Tokyo'  
   end
 end
